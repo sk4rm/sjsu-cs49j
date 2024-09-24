a3ad1b65418a5450f82d0e690bc3218b9af5e077
Zi Hong <74897601+sk4rm@users.noreply.github.com>
2024-09-24 15:12:31 -0700

    Implement MyMatrix with tests

--- assignment1/src/Main.java
+++ assignment1/src/Main.java
@@ -1,14 +1,19 @@
 import assignment1.MyCard;
+import assignment1.MyMatrix;
 import assignment1.MyTime;
+
+import java.sql.SQLOutput;
 
 public class Main {
     public static void main(String[] args) {
+        // MyTime tests
         MyTime.printTimeDifference("0120", "1510");
         System.out.println("\n13 hour(s) 50 minute(s)");
 
         MyTime.printTimeDifference("1635", "0250");
         System.out.println("\n10 hour(s) 15 minute(s)");
 
+        // MyCard tests
         MyCard card1 = new MyCard("10S");
         MyCard card2 = new MyCard("QC");
         MyCard card3 = new MyCard("11H");
@@ -16,5 +21,17 @@ public class Main {
         System.out.println(card1.getSuit());
         System.out.println(card2.getSuit());
         System.out.println(card3.getSuit());
+
+        // MyMatrix tests
+        int[][] array1 = {{2, 4}, {3, 4}};
+        MyMatrix matrix1 = new MyMatrix(array1);
+        array1[0][0] = 5;
+        System.out.println(matrix1);
+
+        int[][] array2 = {{1, 2}, {1, 3}};
+        MyMatrix matrix2 = new MyMatrix(array2);
+        MyMatrix result = matrix1.multiply(matrix2);
+        System.out.println(result);
+
     }
 }
--- assignment1/src/assignment1/MyMatrix.java
+++ assignment1/src/assignment1/MyMatrix.java
@@ -1,4 +1,41 @@
 package assignment1;
 
 public class MyMatrix {
+    private final int[][] matrix;
+
+    public MyMatrix(int[][] array) {
+        matrix = array.clone();
+    }
+
+    public int[][] getMatrix() {
+        return matrix;
+    }
+
+    public MyMatrix multiply(MyMatrix other) {
+        int[][] result = new int[matrix.length][other.matrix[0].length];
+
+        for (int i = 0; i < this.matrix.length; i++) {
+            for (int j = 0; j < other.matrix[i].length; j++) {
+                for (int k = 0; k < other.matrix.length; k++) {
+                    result[i][j] += this.matrix[i][k] * other.matrix[k][j];
+                }
+            }
+        }
+
+        return new MyMatrix(result);
+    }
+
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (int[] row : this.matrix) {
+            for (int cell : row) {
+                sb.append(cell);
+                sb.append(" ");
+            }
+            sb.append("\n");
+        }
+
+        sb.deleteCharAt(sb.length() - 1);
+        return sb.toString();
+    }
 }
