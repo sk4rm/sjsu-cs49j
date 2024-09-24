7b1eaaa1dcc074678151c94d143250b71c0f1945
Zi Hong <74897601+sk4rm@users.noreply.github.com>
2024-09-24 13:08:55 -0700

    Update MyTime and add tests

--- assignment1/src/Main.java
+++ assignment1/src/Main.java
@@ -1,5 +1,20 @@
+import assignment1.MyCard;
+import assignment1.MyTime;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.print("TODO Test");
+        MyTime.printTimeDifference("0120", "1510");
+        System.out.println("\n13 hour(s) 50 minute(s)");
+
+        MyTime.printTimeDifference("1635", "0250");
+        System.out.println("\n10 hour(s) 15 minute(s)");
+
+        MyCard card1 = new MyCard("10S");
+        MyCard card2 = new MyCard("QC");
+        MyCard card3 = new MyCard("11H");
+
+        System.out.println(card1.getSuit());
+        System.out.println(card2.getSuit());
+        System.out.println(card3.getSuit());
     }
 }
--- assignment1/src/assignment1/MyTime.java
+++ assignment1/src/assignment1/MyTime.java
@@ -1,4 +1,21 @@
 package assignment1;
 
 public class MyTime {
+    public static void printTimeDifference(String time1, String time2) {
+        int parsed1 = Integer.parseInt(time1);
+        int parsed2 = Integer.parseInt(time2);
+
+        int hour1 = parsed1 / 100;
+        int hour2 = parsed2 / 100;
+
+        int min1 = parsed1 % 100 + hour1 * 60;
+        int min2 = parsed2 % 100 + hour2 * 60;
+        if (min2 < min1) min2 += 24 * 60;
+
+        int minDiff = min2 - min1;
+        int hourDiff = minDiff / 60;
+        minDiff = minDiff % 60;
+
+        System.out.printf("%d hour(s) %d minute(s)", hourDiff, minDiff);
+    }
 }
